fb42704f2cf34475154ecc5908c4e833504c6981
Friad2K <114339553+Friad2K@users.noreply.github.com>
2022-12-26 15:22:41 +0000

    Library of Includes added. Main function(Empty).
    Function created that defines the Card.
    
    Starting point

--- CardGameOOP/Main.cpp
+++ CardGameOOP/Main.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+
+// That defines a card - they must have suit, name and value
+struct Card
+{
+    string card_name;
+    string suit;
+    int value;
+};
+
 int main() {
 
 }
