ba00893d3fcd0364d297fc92b5b6983a1d41a337
Friad2K <114339553+Friad2K@users.noreply.github.com>
2022-12-26 18:06:36 +0000

    I have added a new pointer called Card which point at card_to_guess and it access the members of the object using the arrow operator
    I hvae used vector to creat a dynamic array of cards

--- CardGameOOP/Main.cpp
+++ CardGameOOP/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <vector> // Include the vector header
 
 using namespace std;
 
@@ -13,20 +14,20 @@ struct Card
 };
 
 // Function prototypes
-void convert_card(Card& theCard, int theNumber);
-void get_suit(Card& theCard, int theNumber);
+void convert_card(Card* theCard, int theNumber);
+void get_suit(Card* theCard, int theNumber);
 
 int main() {
     // Seed the random number generator
     srand((unsigned)time(0));
 
-    // Use a pointer to create an array of cards
-    Card* pack;
-    pack = new Card[52]; // 52 cards in a pack
+    // Use a vector to create a dynamic array of cards
+    vector<Card> pack;
+    pack.resize(52); // 52 cards in a pack
 
     int guess_value;
     int card_to_guess_num;
-    Card card_to_guess;
+    Card* card_to_guess;
     int num_guesses = 0;
     bool not_guessed = true;
 
@@ -34,31 +35,31 @@ int main() {
     for (int i = 0; i < 52; i++)
     {
         int cardNum = (rand() % 13) + 1; // 13 possible cards
-        convert_card(*(pack + i), cardNum);
+        convert_card(&pack[i], cardNum); // Pass a pointer to the card in the vector
 
         int suitNum = (rand() % 4) + 1; // 4 possible suits
-        get_suit(*(pack + i), suitNum);
+        get_suit(&pack[i], suitNum); // Pass a pointer to the card in the vector
     }
 
     card_to_guess_num = (rand() % 52); // Randomly select a card for the user to guess
-    card_to_guess = pack[card_to_guess_num];
-    cout << "Guess the value of the card: " << card_to_guess.card_name << " of " << card_to_guess.suit << endl;
+    card_to_guess = &pack[card_to_guess_num]; // Get a pointer to the chosen card
+    cout << "Guess the value of the card: " << card_to_guess->card_name << " of " << card_to_guess->suit << endl;
     while (not_guessed)
     { // Keep looping until the user guesses the correct value
         cout << "Enter your guess: ";
         cin >> guess_value;
         num_guesses++;
-        if (guess_value < card_to_guess.value)
+        if (guess_value < card_to_guess->value)
         {
             cout << "Higher!" << endl;
         }
-        else if (guess_value > card_to_guess.value)
+        else if (guess_value > card_to_guess->value)
         {
             cout << "Lower!" << endl;
         }
         else
         {
-            cout << "Correct! The card was the " << card_to_guess.card_name << " of " << card_to_guess.suit << endl;
+            cout << "Correct! The card was the " << card_to_guess->card_name << " of " << card_to_guess->suit << endl;
             not_guessed = false;
         }
     }
@@ -69,79 +70,82 @@ int main() {
 
 // Declare function called convert_card that takes a reference to a card object called the card
 // And an integer called thenumber as arguments
-void convert_card(Card& theCard, int theNumber)
+void convert_card(Card* theCard, int theNumber)
 {
     switch (theNumber)
     {
     case 1:
-        theCard.card_name = "Ace";
-        theCard.value = 1;
+        theCard->card_name = "Ace";
+        theCard->value = 1;
         break;
     case 2:
-        theCard.card_name = "2";
-        theCard.value = 2;
+        theCard->card_name = "2";
+        theCard->value = 2;
         break;
     case 3:
-        theCard.card_name = "3";
-        theCard.value = 3;
+        theCard->card_name = "3";
+        theCard->value = 3;
         break;
     case 4:
-        theCard.card_name = "4";
-        theCard.value = 4;
+        theCard->card_name = "4";
+        theCard->value = 4;
         break;
     case 5:
-        theCard.card_name = "5";
-        theCard.value = 5;
+        theCard->card_name = "5";
+        theCard->value = 5;
         break;
     case 6:
-        theCard.card_name = "6";
-        theCard.value = 6;
+        theCard->card_name = "6";
+        theCard->value = 6;
         break;
     case 7:
-        theCard.card_name = "7";
-        theCard.value = 7;
+        theCard->card_name = "7";
+        theCard->value = 7;
         break;
     case 8:
-        theCard.card_name = "8";
-        theCard.value = 8;
+        theCard->card_name = "8";
+        theCard->value = 8;
         break;
     case 9:
-        theCard.card_name = "9";
-        theCard.value = 9;
+        theCard->card_name = "9";
+        theCard->value = 9;
         break;
     case 10:
-        theCard.card_name = "10";
-        theCard.value = 10;
+        theCard->card_name = "10";
+        theCard->value = 10;
         break;
     case 11:
-        theCard.card_name = "Jack";
-        theCard.value = 10;
+        theCard->card_name = "Jack";
+        theCard->value = 10;
         break;
     case 12:
-        theCard.card_name = "Queen";
-        theCard.value = 11;
+        theCard->card_name = "Queen";
+        theCard->value = 10;
         break;
     case 13:
-        theCard.card_name = "King";
-        theCard.value = 12;
+        theCard->card_name = "King";
+        theCard->value = 10;
         break;
     }
 }
-void get_suit(Card& theCard, int theNumber)
+
+// Declare function called get_suit that takes a reference to a card object called theCard
+// And an integer called theNumber as arguments
+void get_suit(Card* theCard, int theNumber)
 {
     switch (theNumber)
     {
     case 1:
-        theCard.suit = "Spades";
+        theCard->suit = "Spades";
         break;
     case 2:
-        theCard.suit = "Hearts";
+        theCard->suit = "Hearts";
         break;
     case 3:
-        theCard.suit = "Clubs";
+        theCard->suit = "Clubs";
         break;
     case 4:
-        theCard.suit = "Diamonds";
+        theCard->suit = "Diamonds";
         break;
     }
 }
